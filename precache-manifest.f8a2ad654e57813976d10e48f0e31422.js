6a9ada6dce34ad6182d19c6684d6e34eff03fd8e
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-07-18 10:06:28 +0300

    Updates

---
+++ precache-manifest.f8a2ad654e57813976d10e48f0e31422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358afc5570f1a6acd01aa0c577fb728f",
+    "url": "/react_calculator/index.html"
+  },
+  {
+    "revision": "5445207c01a98a7b03d9",
+    "url": "/react_calculator/static/css/main.b8896d66.chunk.css"
+  },
+  {
+    "revision": "798b8cae4ba6dd68b8eb",
+    "url": "/react_calculator/static/js/2.4260dc4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_calculator/static/js/2.4260dc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5445207c01a98a7b03d9",
+    "url": "/react_calculator/static/js/main.4d8e4168.chunk.js"
+  },
+  {
+    "revision": "db6ddcca8ffe7cf6f1d0",
+    "url": "/react_calculator/static/js/runtime-main.1c7ea57c.js"
+  }
+]);
